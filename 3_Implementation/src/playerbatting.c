113d23e68d29bdf452b49881ede46c327f8029b9
srikanthbandi949 <srikanthreddy2000b@gmail.com>
2022-02-09 18:12:06 +0530

    code update

---
+++ 3_Implementation/src/playerbatting.c
@@ -0,0 +1,92 @@
+#include<stdio.h>
+#include<string.h>
+#include<time.h>
+#include<stdlib.h>
+#include "handcricket.h"
+
+
+int batting(char battingplayer[],char bowlingplayer[],int oversToplay)
+{
+ int i,j,battingScore,sum=0,bowlerSocre,flag=1;
+ char c;
+ if(oversToplay>0)
+ {
+  for(i=1;i<=oversToplay;i++)
+  {
+   for(j=1;j<=6;j++)
+   {
+    printf("%d.%d ball\n",i,j);
+    printf("%s Press Enter key to bowl :",bowlingplayer);
+    fflush(stdin);
+    scanf("%c",&c);
+    fflush(stdin);
+    bowlerSocre=(int)rand()%7;//Generate from 0-6 where 0 means no ball
+    printf("%s Press Enter key to bat :",battingplayer);
+    fflush(stdin);
+    scanf("%c",&c);
+    fflush(stdin);
+    battingScore=rand()%7;//Generate 0-6 runs
+    if(battingScore==bowlerSocre)
+    {
+     flag=0;
+     printf("--------------------->  %s is Out  <-----------------------\n",battingplayer);
+     printf("%s Total Score=%d\n",battingplayer,sum);
+     if(sum==0)
+            printf("--------------------->  Hehehe Duck Out  <-----------------------\n");
+     break;
+    }
+    else
+    {
+     printf("Got %d runs\n",battingScore);
+                    if(battingScore==6)
+                    printf("Sixerrrrrr Keep it Up\n");
+                    if(battingScore==4)
+               printf("Its a Four\n");
+     sum=sum+battingScore;
+     printf("%s Score=%d\n",battingplayer,sum);
+    }
+   }
+   if(flag==0)
+   break;
+  }
+ 
+  return(sum);
+ }
+   if(oversToplay==0)
+   {
+      while(flag)
+      {
+        printf("%s Press Enter key to bowl :",bowlingplayer);
+  fflush(stdin);
+  scanf("%c",&c);
+  fflush(stdin);
+  bowlerSocre=(int)rand()%7;//Generate from 0-6 where 0 means no ball
+  printf("%s Press Enter key to bat :",battingplayer);
+  fflush(stdin);
+  scanf("%c",&c);
+  fflush(stdin);
+  battingScore=rand()%7;//Generate 0-6 runs
+  if(battingScore==bowlerSocre)
+  {
+   flag=0;
+   printf("--------------------->  %s is Out  <------------------->\n",battingplayer);
+   printf("%s Total Score=%d\n",battingplayer,sum);
+   if(sum==0)
+   printf("--------------------->  Hehehe Duck Out  <---------------------\n");
+   break;
+  }
+  else
+  {
+   printf("Got %d runs\n",battingScore);
+            if(battingScore==6)
+            printf("Sixerrrrrr Keep it Up\n");
+            if(battingScore==4)
+       printf("Its a Four Great Man\n");
+   sum+=battingScore;
+   printf("%s Score=%d\n",battingplayer,sum);
+  }
+      }
+      return(sum);
+   }
+ 
+}
---
+++ 3_Implementation/src/playertoss.c
@@ -0,0 +1,102 @@
+#include<stdio.h>
+#include<string.h>
+#include<time.h>
+#include<stdlib.h>
+#include "handcricket.h"
+
+int PlayerToss(char player1[],char player2[],int oversToplay)
+   {
+      int player1Choice,player2Choice,toss,player1Toss,batorbowlChoice,player2Score,player1Score;
+      printf("%s --> Choose your choice either 1 for Head or 2 for Tail : ",player1);
+      scanf("%d",&player1Choice);
+      if(player1Choice==1)
+      {
+            player2Choice=2;
+            printf("%s -> Choice is Tail\n",player2);
+      }
+      else if(player1Choice==2)
+      {
+         player2Choice=1;
+         printf("%s -> Choice is Head\n",player2);
+      }
+      else
+      {
+         printf("------------------------------------------>  Invalid Choice  <------------------------------------------\n");
+         return(0);
+      }
+      toss=rand()%2;
+      toss=toss+1;
+      if(toss==1){
+         printf("Toss is Head\n");
+      }
+      else{
+         printf("Toss is Tail\n");
+      }
+
+      if(toss==player1Choice)
+      {
+         player1Toss=1;
+         printf("Player 1 %s Won the Toss\n",player1);
+         printf("Choose\n1 - Batting\n2 - Bowl: ");
+         scanf("%d",&batorbowlChoice);
+         if(batorbowlChoice==1)
+         {
+            printf("--------------------->  %s is Going to bat Now  <-----------------------\n",player1);
+            player1Score=batting(player1,player2,oversToplay);
+            printf("--------------------->  %s is Going to bat Now  <-----------------------\n",player2);
+            player2Score=batting(player2,player1,oversToplay);
+            printf("--------------------->  Its Time for Results  <-----------------------\n");
+         }
+         else
+         {
+            printf("--------------------->  %s is Going to bat Now  <-----------------------\n",player2);
+            player2Score=batting(player2,player1,oversToplay);
+            printf("--------------------->  %s is Going to bat Now  <-----------------------\n",player1);
+            player1Score=batting(player1,player2,oversToplay);
+            printf("--------------------->  Its Time for Results  <-----------------------\n");
+         }
+      }
+      else if(toss==player2Choice)
+      {
+         player1Toss=2;
+         printf("Player 2 %s Won the Toss\n",player2);
+         printf("\n1 - Batting\n2 - Bowl\nChoose : ");
+         scanf("%d",&batorbowlChoice);
+         if(batorbowlChoice==1)
+         {
+            printf("--------------------->  %s is Going to bat Now  <-----------------------\n",player2);
+            player2Score=batting(player2,player1,oversToplay);
+            printf("--------------------->  %s is Going to bat Now  <-----------------------\n",player1);
+            player1Score=batting(player1,player2,oversToplay);
+            printf("--------------------->  Its Time for Results  <-----------------------\n");
+         }
+         else
+         {
+            printf("--------------------->  %s is Going to bat Now  <-----------------------\n",player1);
+            player1Score=batting(player1,player2,oversToplay);
+            printf("--------------------->  %s is Going to bat Now  <-----------------------\n",player2);
+            player2Score=batting(player2,player1,oversToplay);
+            printf("--------------------->  Its Time for Results  <-----------------------\n");
+         }
+      }
+      else
+      {
+         printf("---------------------------------------------->  Error  <-----------------------------------------------\n");
+         return(0);
+      }
+      if(player1Score==player2Score)
+      {
+         printf("Draw Match\nThanks For Playing the Game\n");
+      }
+      if(player1Score>player2Score){
+         printf("%s  score is : %d\n",player1,player1Score);
+         printf("%s  score is : %d\n\n",player2,player2Score);
+         printf("%s Won the Match\n\nThanks for Playing the Game\n",player1);
+      }
+      if(player1Score<player2Score){
+         printf("%s  score is : %d\n",player1,player1Score);
+         printf("%s  score is : %d\n\n",player2,player2Score);
+         printf("%s Won the Match\nThanks for Playing the Game\n",player2);
+      }
+      return(player1Toss);
+}
