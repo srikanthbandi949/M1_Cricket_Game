b349a0fea95ac1c96da224227d2927591de599bb
srikanthbandi949 <srikanthreddy2000b@gmail.com>
2022-02-09 12:15:37 +0530

    update code

---
+++ 3_Implemetation/version1/hadcricket.h
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string.h>
+#include<time.h>
+#include<stdlib.h>
+
+int GetPlayerToss(char player1[],char player2[],int oversToplay);
+int batting(char battingplayer[],char bowlingplayer[],int oversToplay);
+int GetPlayerToss(char player1[],char player2[],int oversToplay);
+int batting(char battingplayer[],char bowlingplayer[],int oversToplay);
